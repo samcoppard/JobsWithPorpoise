a3a008ffacf89a1a5944ae157e9060ad9ce35aef
Sam Coppard <samcoppard@Sams-MacBook-Pro.local>
2023-06-21 14:38:00 +0100

    Format with Black

--- synchronise.py
+++ synchronise.py
@@ -1,13 +1,13 @@
 import os
 
-# Create a method to run individual files
 
+# Create a method to run individual files
 def runpy(filename):
-    string = 'python3 ' + './JobsWithPorpoise/' + filename
+    string = "python3 " + "./JobsWithPorpoise/" + filename
     os.system(string)
 
-# Create a method to run everything
 
+# Create a method to run everything
 def scrape_to_postgres_to_webflow():
     # Run the file that scrapes jobs
     runpy("scraping.py")
@@ -16,15 +16,19 @@ def scrape_to_postgres_to_webflow():
     runpy("categorisation.py")
 
     # Run the file that updates Postgres and Webflow with removed jobs
+    print("Going through old jobs...")
     runpy("old_jobs.py")
 
     # Run the file that updates Postgres and Webflow with new jobs
+    print("Going through new jobs...")
     runpy("new_jobs.py")
 
     # Run the file that updates organisations in Postgres
+    print("Updating orgs in PSQL...")
     runpy("psql_org_update.py")
 
     # Run the file that syncs orgs in Webflow CMS with orgs in psql
+    print("Syncing orgs from PSQL to Webflow")
     runpy("org_sync.py")
 
 
