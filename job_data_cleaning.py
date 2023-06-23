c5eb9f108bbac27e1bc52641ddc325307e9bee9c
Sam Coppard <samcoppard@Sams-MacBook-Pro.local>
2023-06-23 16:43:36 +0100

    Format with Black

--- job_data_cleaning.py
+++ job_data_cleaning.py
@@ -4,21 +4,26 @@ import pandas as pd
 import yaml
 
 # Pull in the scraped jobs as a dataframe
-scraped_csv = pd.read_csv('scraped_jobs.csv')
+scraped_csv = pd.read_csv("scraped_jobs.csv")
 scraped_jobs = pd.DataFrame(scraped_csv)
+
 
 def remove_incomplete_rows(df):
     """Remove all rows with a missing value anywhere, then reset the index"""
-    df.dropna(how='any', axis=0, inplace=True)
+    df.dropna(how="any", axis=0, inplace=True)
     df.reset_index(drop=True, inplace=True)
 
 
 def clean_locations(df, column):
     """Create a new column to store locations in title case, and deal with a couple
-        of edge cases that mess up location mapping"""
-    df[column] = (df[column].str.title()
-    # Remove mentions of New York and New South Wales
-    .str.replace("New York", "").str.replace("New South Wales", ""))
+    of edge cases that mess up location mapping"""
+    df[column] = (
+        df[column]
+        # Convert to title case
+        .str.title()
+        # Remove mentions of New York and New South Wales
+        .str.replace("New York", "").str.replace("New South Wales", "")
+    )
     # Change "Sale" & "Bury" to "Manchester" (by using a boolean mask)
     mask = df[column].isin(["Sale", "Bury"])
     df.loc[mask, column] = "Manchester"
@@ -28,7 +33,15 @@ def clean_job_titles(df, column):
     """Remove useless phrases & characters; limit to 255 chars; tidy up punctuation"""
 
     # Get rid of extra phrases that aren't needed
-    phrases_to_remove = ["(All Genders)", "(all genders)", "all genders", " - Permanent (no closing date – apply now)", "/ ANNUM ", "docx", "Job Description"]
+    phrases_to_remove = [
+        "(All Genders)",
+        "(all genders)",
+        "all genders",
+        " - Permanent (no closing date – apply now)",
+        "/ ANNUM ",
+        "docx",
+        "Job Description",
+    ]
     for phrase in phrases_to_remove:
         df[column] = df[column].str.replace(phrase, "")
 
@@ -37,31 +50,33 @@ def clean_job_titles(df, column):
     for char in chars_to_remove:
         df[column] = df[column].str.replace(char, "")
 
-    # Limit the 'Job Title' column to 255 characters (the maximum allowed in the jobs PSQL table)
+    # Limit the 'Job Title' column to 255 characters (to match length in PSQL)
     df[column] = df[column].str[:255]
 
-    # Tidy up hyphens
-    df[column] = (df[column].str.replace("- ", " - ").str.replace(" -", " - ").str.replace("  ", " ")
-
-    # Tidy up colons
-    .str.replace(" :", ": ").str.replace("  ", " "))
-
-    # Replace a weird apostrophe with a normal one
-    df[column] = df[column].str.replace("’", "'")
-
-    # Remove whitespace at the end
-    df[column] = (df[column].str.strip()
-
-    # Get rid of trailing punctuation
-    .str.replace(r'[:,(/-]+$', '', regex=True)
-    
-    # Remove extra spaces again (including mid-string this time)
-    .str.strip().str.replace("  ", " "))
+    df[column] = (
+        df[column]
+        # Tidy up hyphens
+        .str.replace("- ", " - ")
+        .str.replace(" -", " - ")
+        .str.replace("  ", " ")
+        # Tidy up colons
+        .str.replace(" :", ": ")
+        .str.replace("  ", " ")
+        # Replace a weird apostrophe with a normal one
+        .str.replace("’", "'")
+        # Remove whitespace at the end
+        .str.strip()
+        # Get rid of trailing punctuation
+        .str.replace(r"[:,(/-]+$", "", regex=True)
+        # Remove extra spaces again (including mid-string this time)
+        .str.strip()
+        .str.replace("  ", " ")
+    )
 
 
 def load_exclusions():
-    """ Load the list of words to exclude from being capitalized from the YAML file """
-    with open('./JobsWithPorpoise/capital_exclusions.yaml', 'r') as file:
+    """Load the list of words to exclude from being capitalized from the YAML file"""
+    with open("./JobsWithPorpoise/capital_exclusions.yaml", "r") as file:
         return yaml.safe_load(file)
 
 
@@ -70,36 +85,42 @@ exclusions = load_exclusions()
 
 
 def convert_to_title_case(string):
-    """ Change the string to title case, except where you wouldn't actually want to """
+    """Change the string to title case, except where you wouldn't actually want to"""
     words = string.split()
     capitalized_words = [
-        word.capitalize() if word not in exclusions else word for word in words]
-    capitalized_string = ' '.join(capitalized_words)
+        word.capitalize() if word not in exclusions else word for word in words
+    ]
+    capitalized_string = " ".join(capitalized_words)
     # Fix an edge case with Next.js
     capitalized_string = capitalized_string.replace("Nextjs", "Next.js")
     return capitalized_string
 
 
 def convert_df_column_to_title_case(df, column):
-    """ Change a df column of string values to title case """
+    """Change a df column of string values to title case"""
     df[column] = df[column].apply(convert_to_title_case)
 
+
 remove_incomplete_rows(scraped_jobs)
-clean_locations(scraped_jobs, 'Location')
-clean_job_titles(scraped_jobs, 'Job Title')
-convert_df_column_to_title_case(scraped_jobs, 'Job Title')
+clean_locations(scraped_jobs, "Location")
+clean_job_titles(scraped_jobs, "Job Title")
+convert_df_column_to_title_case(scraped_jobs, "Job Title")
 
 
+def create_new_columns(df):
+    # Create a new column for concatenating org - role - location, limited to 255 chars
+    scraped_jobs["concat"] = (
+        scraped_jobs["Company"]
+        + " - "  # Looks clunky but can't use f-strings here
+        + scraped_jobs["Job Title"]
+        + " - "
+        + scraped_jobs["Location"]
+    ).str[:255]
 
-# Create a new column for concatenating org - role - location, and limit it to 255 chars
-scraped_jobs['concat'] = (scraped_jobs['Company'] + " - " + scraped_jobs[
-    'Job Title'] + " - " + scraped_jobs['Location']).str[:255]
+    # Create new columns for the jobs' mapped locations, job types, and seniorities
+    scraped_jobs["mapped_location"] = "not mapped"
+    scraped_jobs["job_types"] = "not mapped"
+    scraped_jobs["seniority"] = "mid level"
 
 
-# Create new columns for the jobs' mapped locations, job types, and seniorities
-scraped_jobs['mapped_location'] = "not mapped"
-scraped_jobs['job_types'] = "not mapped"
-scraped_jobs['seniority'] = "mid level"
-
-
-scraped_jobs.to_csv("cleaned_jobs.csv", index=False)
+scraped_jobs.to_csv("categorised_jobs.csv", index=False)
