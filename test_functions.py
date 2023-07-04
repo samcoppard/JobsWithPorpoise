579b7fe6d44c2a064a559b333c7926f471ac5103
Sam Coppard <samcoppard@Sams-MacBook-Pro.local>
2023-07-04 15:59:40 +0100

    First unit test

--- test_functions.py
+++ test_functions.py
@@ -10,19 +10,16 @@ def test_get_mapping_keywords():
         # Create temporary YAML files with test data
         file1 = os.path.join(temp_dir, "file1.yaml")
         with open(file1, "w") as f:
-            f.write("key1:\n - value1\n - value2\n")
+            f.write("- key1\n- value1\n- value2\n")
         file2 = os.path.join(temp_dir, "file2.yaml")
         with open(file2, "w") as f:
-            f.write("key2:\n - value3\n - value4\n")
+            f.write("- key2\n- value3\n- value4\n")
         
         # Call the function with the test directory
         result = get_mapping_keywords(temp_dir)
-
+        
         # Define the expected output based on the test data
-        expected_output = {
-            "key1": ["value1", "value2"],
-            "key2": ["value3", "value4"]
-        }
+        expected_output = {'key1': ['value1', 'value2'], 'key2': ['value3', 'value4']}
 
         # Assert that the result matches the expected output
         assert result == expected_output
